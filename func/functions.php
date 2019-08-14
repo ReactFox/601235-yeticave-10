bee21d7e5ee35eec12b7eeb58eb7657ecaff0e98
ReactFox <itboleslav@gmail.com>
2019-08-14 19:42:04 +0300

    m3-t1 создаёт шаблоны, защищает от XSS

---
+++ func/functions.php
@@ -0,0 +1,15 @@
+<?php
+function amount_formatting($bet)
+{
+    $bet = ceil($bet);
+    $currency_sign = '<b class="rub">р</b>';
+    $result = '';
+
+    if ($bet > 0 && $bet < 1000) {
+        $result = $bet;
+    }
+    if ($bet >= 1000) {
+        $result = number_format($bet, 0, '', ' ');
+    }
+    return $result . $currency_sign;
+}
