7c54b0647260e351a28777fd79e89af506d1e312
ReactFox <itboleslav@gmail.com>
2019-08-16 02:07:06 +0300

    создаёт функцию формата даты и вывода класса в случае если осталось минут меньше часа

--- func/functions.php
+++ func/functions.php
@@ -13,3 +13,30 @@ function amount_formatting($bet)
     }
     return $result . $currency_sign;
 }
+
+
+function stop_time($final_date)
+{
+    $time = '';
+    $date_now = date_create('now');
+    $date_bate = date_create($final_date);
+    $date_diff = date_diff($date_bate, $date_now);
+    $hour = date_interval_format($date_diff, '%d %H %I');
+
+    $time = explode(' ', $hour);
+    $days_left = $time[0];
+
+/*  расшифровка моего индийского кода (ниже 32строчки) для Наставника.
+    если в массиве тайм больше значение дней больше 0,
+    то в ячейку где минуты присваиваем число дней умножить на 24 часа,
+    плюс текущие значение минут, если в 24 строке кода формат указать '%H %I',
+    то нарушиться логика подсчёта минут, и если до финальной ставки будет например один день и сколько-то часов,
+    то возратиться только результат в часах и минутах, а дни не учтуться*/
+
+//    TODO потом удалить
+
+    if ($days_left > 0) {
+        $time[1] = ($days_left * 24) + $time[1];
+    }
+    return $time;
+}
