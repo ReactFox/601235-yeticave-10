a2b0f5289df99875c5fe8b0e797756b02ce05a11
ReactFox <itboleslav@gmail.com>
2019-08-21 18:29:27 +0300

    m5-t1 Читает и показывает записи при помощи PHP сценариев из MySQL

---
+++ config/init.php
@@ -0,0 +1,10 @@
+<?php
+$db = [
+    'host' => '601235-yeticave-10',
+    'user' => 'root',
+    'password' => '',
+    'database' => 'yeticave_db'
+];
+
+$con = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
+mysqli_set_charset($con, 'utf8');
--- index.php
+++ index.php
@@ -1,9 +1,46 @@
 <?php
 error_reporting(-1);
-
+require_once 'config/init.php';
 require_once 'data/data.php';
 require_once 'func/functions.php';
 require_once 'helpers.php';
+
+
+if (!$con) {
+    $error = mysqli_connect_error();
+    echo $error;
+    exit;
+}
+
+//получает категории
+$sql = "SELECT category_title, symbolic_code FROM categories";
+
+$result = mysqli_query($con, $sql);
+
+if ($result) {
+    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+} else {
+    $error = mysqli_error($con);
+    echo $error;
+}
+
+//получает список лотов на главной
+$sql = "SELECT lot_title, lot_image, starting_price, category_title, date_finish
+FROM lots l
+         JOIN categories c ON l.category_id = c.id
+WHERE date_finish > NOW()
+ORDER BY date_creation DESC LIMIT 9";
+
+$result = mysqli_query($con, $sql);
+
+if ($result) {
+    $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
+} else {
+    $error = mysqli_error($con);
+    echo $error;
+}
+
+mysqli_close($con);
 
 $page_content = include_template('main.php', [
     'categories' => $categories,
