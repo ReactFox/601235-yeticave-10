42c0e0d23fb75fe87a7fd89442ef7e7dc07c241f
ReactFox <itboleslav@gmail.com>
2019-08-23 23:29:23 +0300

    m6-t1 начало задания, подключения leyout.php lot.php

---
+++ lot.php
@@ -0,0 +1,40 @@
+<?php
+error_reporting(-1);
+require_once 'config/init.php';
+require_once 'data/data.php';
+require_once 'func/functions.php';
+require_once 'helpers.php';
+
+
+if (!$con) {
+    $error = mysqli_connect_error();
+    echo $error;
+    exit;
+}
+
+//получает категории
+$sql = "SELECT category_title, symbolic_code FROM categories";
+
+$result = mysqli_query($con, $sql);
+
+if ($result) {
+    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+} else {
+    $error = mysqli_error($con);
+    echo $error;
+}
+
+$page_content = include_template('lot.php', [
+//    'categories' => $categories,
+]);
+
+
+$layout_content = include_template('layout.php', [
+    'content' => $page_content,
+    'categories' => $categories,
+    'title' => $title,
+    'is_auth' => $is_auth,
+    'user_name' => $user_name,
+]);
+
+print($layout_content);
