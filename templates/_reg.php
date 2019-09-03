afc5a0dd0e71e653c854267b6c0340fa3baaabc6
ReactFox <itboleslav@gmail.com>
2019-09-03 19:53:16 +0300

    m7-t1 добавляет заполнение формы

--- templates/_reg.php
+++ templates/_reg.php
@@ -14,30 +14,32 @@
       autocomplete="off">
     <h2>Регистрация нового аккаунта</h2>
     <?php $field_email_error = isset($errors['email']) ? 'form__item--invalid' : ''; ?>
-    <div class="form__item <?= $field_email_error ?>"> <!-- form__item--invalid -->
+    <div class="form__item <?= $field_email_error ?>">
         <label for="email">E-mail <sup>*</sup></label>
-        <input id="email" type="text" name="email" placeholder="Введите e-mail">
+        <input id="email" type="text" name="email" value="<?= getPostVal('email') ?>" placeholder="Введите e-mail">
         <span class="form__error"><?= $errors['email'] ?></span>
     </div>
 
     <?php $field_pass_error = isset($errors['password']) ? 'form__item--invalid' : ''; ?>
     <div class="form__item <?= $field_pass_error ?>">
         <label for="password">Пароль <sup>*</sup></label>
-        <input id="password" type="password" name="password" placeholder="Введите пароль">
+        <input id="password" type="password" name="password" value="<?= getPostVal('password') ?>"
+               placeholder="Введите пароль">
         <span class="form__error"><?= $errors['password'] ?></span>
     </div>
 
     <?php $field_name_error = isset($errors['user_name']) ? 'form__item--invalid' : ''; ?>
     <div class="form__item <?= $field_name_error ?>">
         <label for="name">Имя <sup>*</sup></label>
-        <input id="name" type="text" name="user_name" placeholder="Введите имя">
+        <input id="name" type="text" name="user_name" value="<?= getPostVal('user_name') ?>" placeholder="Введите имя">
         <span class="form__error"><?= $errors['user_name'] ?></span>
     </div>
 
     <?php $field_contacts_error = isset($errors['contacts']) ? 'form__item--invalid' : ''; ?>
     <div class="form__item <?= $field_contacts_error ?>">
         <label for="message">Контактные данные <sup>*</sup></label>
-        <textarea id="message" name="contacts" placeholder="Напишите как с вами связаться"></textarea>
+        <textarea id="message" name="contacts"
+                  placeholder="Напишите как с вами связаться"><?= getPostVal('user_name') ?></textarea>
         <span class="form__error"><?= $errors['contacts'] ?></span>
     </div>
 
