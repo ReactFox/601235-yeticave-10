dadcfd70dd698efb318e3eb9e9c9ee84a25fa529
ReactFox <itboleslav@gmail.com>
2019-09-28 20:04:47 +0300

    m9-t1 Отправка письма

--- getwinner.php
+++ getwinner.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(-1);
 require_once 'vendor/autoload.php';
 
 $sql = 'SELECT * FROM lots l WHERE date_finish < NOW()
@@ -30,18 +31,6 @@ if (!empty($result)) {
 }
 
 
-//$transport = new Swift_SmtpTransport('smtp.gmail.com', 25);
-//$transport->setUsername('testphpserver81@gmail.com');
-//$transport->setPassword('a123456789-');
-//$mailer = new Swift_Mailer($transport);
-
-$transport = (new Swift_SmtpTransport('smtp.mailtrap.io', 25))
-    ->setUsername('a3da5977d46c35')
-    ->setPassword('840f2b35fc3e2e');
-
-$mailer = new Swift_Mailer($transport);
-
-
 $sql = 'SELECT winner_id, user_name, l.id AS lot_win_id, lot_title, email
         FROM lots l JOIN users u ON winner_id = u.id WHERE winner_id IS NOT NULL';
 
@@ -50,36 +39,26 @@ if ($result && mysqli_num_rows($result)) {
     $lots_win = mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
+$transport = (new Swift_SmtpTransport('phpdemo.ru', 25))
+    ->setUsername('keks@phpdemo.ru')
+    ->setPassword('htmlacademy');
+
+$mailer = new Swift_Mailer($transport);
 
 foreach ($lots_win as $lot_win) {
     $recipient = [];
     $recipient[$lot_win['email']] = $lot_win['user_name'];
 
-//    $message = new Swift_Message();
-//    $message->setSubject("Ваша ставка победила");
-//    $message->setFrom(['keks@phpdemo.ru' => 'Yeticave']);
-//    $message->setTo($recipient);
-
-
-    $message = (new Swift_Message('Ваша ставка победила'))
-        ->setFrom(['keks@phpdemo.ru' => 'Yeticave'])
-        ->setTo($recipient)
-        ->setBody($msg_content = include_template('_email.php', [
-            'lot_win' => $lot_win
-//        'user_name' => $lot_win['user_name'],
-//        'lot_title' => $lot_win['lot_title'],
-//        'lot_win_id' => $lot_win['lot_win_id'],
-        ]));
+//    $message = (new Swift_Message('Ваша ставка победила'))
+//        ->setFrom(['keks@phpdemo.ru' => 'Yeticave'])
+//        ->setTo($recipient)
+//        ->setReturnPath('saltankin@mail.ru')
+//        ->setBody($msg_content = include_template('_email.php', [
+//            'lot_win' => $lot_win
+//        ]));
     unset($recipient);
 
-//    $msg_content = include_template('_email.php', [
-//        'lot_win' => $lot_win
-//        'user_name' => $lot_win['user_name'],
-//        'lot_title' => $lot_win['lot_title'],
-//        'lot_win_id' => $lot_win['lot_win_id'],
-//    ]);
-//    $message->setBody($msg_content, 'text/html');
-    $res = $mailer->send($message);
+//    $res = $mailer->send($message);
 
     if ($res) {
         print("Рассылка успешно отправлена");
