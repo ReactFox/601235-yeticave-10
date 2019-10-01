3447bc85babe83c8f8ceeb3610f3209652c1a9b1
ReactFox <itboleslav@gmail.com>
2019-10-01 15:53:49 +0300

    попытка2 исправляет Б-16

--- templates/_lot.php
+++ templates/_lot.php
@@ -9,26 +9,26 @@
     </ul>
 </nav>
 <section class="lot-item container">
-    <h2><?= $lot['lot_title'] ?></h2>
+    <h2><?= htmlspecialchars($lot['lot_title']) ?></h2>
     <div class="lot-item__content">
         <div class="lot-item__left">
             <div class="lot-item__image">
-                <img src="../uploads/<?= $lot['lot_image'] ?>" width="730" height="548"
-                     alt="Изображение: <?= $lot['lot_title'] ?>">
+                <img src="../uploads/<?= htmlspecialchars($lot['lot_image']) ?>" width="730" height="548"
+                     alt="Изображение: <?= htmlspecialchars($lot['lot_title']) ?>">
             </div>
-            <p class="lot-item__category">Категория: <span><?= $lot['category_title'] ?></span></p>
-            <p class="lot-item__description"><?= $lot['lot_description'] ?></p>
+            <p class="lot-item__category">Категория: <span><?= htmlspecialchars($lot['category_title']) ?></span></p>
+            <p class="lot-item__description"><?= htmlspecialchars($lot['lot_description']) ?></p>
         </div>
         <div class="lot-item__right">
             <div class="lot-item__state">
-                <?php $get_time = stop_time($lot['date_finish']) ?>
+                <?php $get_time = stop_time(htmlspecialchars($lot['date_finish'])) ?>
                 <div class="lot-item__timer timer <?php if ($get_time[1] < '01'): ?>timer--finishing<?php endif; ?>">
                     <?= $get_time[1] . ':' . $get_time[2] ?>
                 </div>
                 <div class="lot-item__cost-state">
                     <div class="lot-item__rate">
                         <span class="lot-item__amount">Текущая цена</span>
-                        <span class="lot-item__cost"><?= amount_formatting($sum_bet['sum_bet']) ?></span>
+                        <span class="lot-item__cost"><?= amount_formatting(htmlspecialchars($sum_bet['sum_bet'])) ?></span>
                     </div>
                     <div class="lot-item__min-cost">
                         Мин. ставка <span><?= htmlspecialchars($lot['bet_step'],
@@ -65,10 +65,10 @@
                     <table class="history__list">
                         <?php foreach ($history_users_bet as $user_bet): ?>
                             <tr class="history__item">
-                                <td class="history__name"><?= $user_bet['user_name'] ?></td>
-                                <td class="history__price"><?= amount_formatting($user_bet['bet_amouth'],
-                                        0) . ' р' ?></td>
-                                <td class="history__time"><?= get_relative_format($user_bet['date_bet']) ?></td>
+                                <td class="history__name"><?= htmlspecialchars($user_bet['user_name']) ?></td>
+                                <td class="history__price"><?= htmlspecialchars(amount_formatting($user_bet['bet_amouth'],
+                                        0)) . ' р' ?></td>
+                                <td class="history__time"><?= htmlspecialchars( get_relative_format($user_bet['date_bet'])) ?></td>
                             </tr>
                         <?php endforeach; ?>
                     </table>
